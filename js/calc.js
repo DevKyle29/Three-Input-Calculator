c2608cc7402f71f1c6cd6cae982768dc0713d496
DevKyle29 <kylebrentfry@gmail.com>
2022-10-13 12:35:10 +0200

    progress
    Calculator functional

--- js/calc.js
+++ js/calc.js
@@ -1,3 +1,18 @@
 function calc(){
-    let input1 = doc
+    let input1 = parseInt(document.getElementById("input1").value);
+    let input2 = parseInt(document.getElementById("input2").value);
+    let oper   = document.getElementById("operators").value;
+
+    if (oper === '+'){
+        document.getElementById("result").value = input1+input2;
+    }
+    if (oper === '-'){
+        document.getElementById("result").value = input1-input2;
+    }
+    if (oper === '*'){
+        document.getElementById("result").value = input1*input2;
+    }
+    if (oper === '/'){
+        document.getElementById("result").value = input1/input2;
+    }
 }
